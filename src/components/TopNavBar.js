3d0d2efb3f5fbf1edefbae6f30a011b066cb3364
Oelshehawi <oelsheha@sfu.ca>
2023-02-01 09:44:28 -0800

    Fontawesome setup

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,9 +1,15 @@
 import * as React from "react";
 import "../style/layout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser)
 
 const TopNavBar = () => {
   return (
     <header className="HeaderBar" >
+      <FontAwesomeIcon id="user-icon" icon="fa-solid user" label="Username" fixedWidth size="7x" />
       TopNavBar
     </header>
   );
