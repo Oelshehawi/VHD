2b303acc641e3d4fc6f7b5fc9a1061d006d59a54
Oelshehawi <oelsheha@sfu.ca>
2023-02-01 09:44:28 -0800

    Fontawesome setup

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import "../style/layout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser)
 
 const SideNavBar = () => {
   return(
   <nav >
-    SideBarssss
+    Side
   </nav>
   );
 };
