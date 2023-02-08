7ea96c7515858b46e341dd512fbee8e279c2c5a8
Oelshehawi <oelsheha@sfu.ca>
2023-02-07 23:00:57 -0800

    The final layout???

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import * as FaIcons from "react-icons/fa";
+import TopNavBar from "../components/TopNavBar"
 
 const IndexPage = () => {
   return (
     <Layout>
+      <TopNavBar  />
       <div className="text"></div>
     </Layout>
   );
