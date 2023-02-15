aae77465cfc7a14715c57fc09aa0de33aa5b49bf
oelsheha <oelsheha@sfu.ca>
2023-02-15 15:46:09 -0800

    Sidebar css and HTML completed

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "../style/layout.css";
+import "../style/topbar.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +7,11 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 library.add(faUser);
 
 const TopNavBar = () => {
-  return <div className="HeaderBar"></div>;
+  return(
+    <nav className="HeaderBar">
+      <div className="text"> Hi there</div>
+    </nav>
+  );
 };
 
 export default TopNavBar;
