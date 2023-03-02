a407b1b4a9792724d38970fb7f154750d62a4fa0
oelsheha <oelsheha@sfu.ca>
2023-03-02 09:49:54 -0800

    Dashboard page html/css and began Schedule page html/css

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "../style/sidebar.css";
+import { Link } from "gatsby";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
 import { FaRegCalendarAlt } from "react-icons/fa";
@@ -15,17 +16,19 @@ const SideNavBar = () => {
         <FaAngleDown id="iconUser" />
       </div>
       <div className="line"></div>
-      <div className="tab">
-        <FaHome className="icon" />
-        {"Dashboard"}
-      </div>
-      <div className="tab">
-        <FaDatabase className="icon" />
-        {"Database "}
-      </div>
-      <div className="tab">
-        <FaRegCalendarAlt className="icon" />
-        {"Schedule"}
+      <div className="links">
+        <Link to="/" className="tab">
+          <FaHome className="icon" />
+          {"Dashboard"}
+        </Link>
+        <Link to="/database" className="tab">
+          <FaDatabase className="icon" />
+          {"Database "}
+        </Link>
+        <Link to="/schedule" className="tab">
+          <FaRegCalendarAlt className="icon" />
+          {"Schedule"}
+        </Link>
       </div>
     </aside>
   );
--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,17 +1,13 @@
 import * as React from "react";
 import "../style/topbar.css";
+import logo from "../images/9925.png";
 
 const TopNavBar = () => {
   return (
     <nav className="HeaderBar">
-      <div className="text">
-        <img
-          src="../images/9925.png"
-          alt="Vancouver Hood Doctors"
-          width="40px"
-          height="400px"
-          margin-left="300px"
-        />
+      <div className="imgcontainer">
+        <img src={logo} alt="Vancouver Hood Doctors" />
+        <div className="text">{"Vancouver Hood Doctors"}</div>
       </div>
       {/* <div className="vLine">
         <div className="vLine2">
