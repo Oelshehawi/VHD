4ff3947398978a7993dfcdea7b7a85d93c543582
oelsheha <oelsheha@sfu.ca>
2023-05-28 15:01:31 -0700

    everything

--- vhd/src/pages/index.js
+++ vhd/src/pages/index.js
@@ -1,54 +1,78 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../components/Layout";
+'use client';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import login from './login.module.css';
+import { FaGoogle } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import axios from "axios";
-import Event from "../components/Event";
-import { API_URL } from "../../../shared/config";
-import { ToastContainer, toast, Slide } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { API_URL } from '../../../shared/config';
 
-const IndexPage = () => {
-  const [events, setEvents] = useState([]);
-  const todaysDate = new Date().toISOString().split("T")[0];
-  const [eventSchedule, setEventSchedule] = useState(false);
-  const [onUpdate, setOnUpdate] = useState(false);
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/events/`)
-      .then((res) => setEvents(res.data))
-      .catch((err) => console.error(err));
-  }, [onUpdate]);
+  const router = useRouter();
 
-  const showDeleteEventToast = () => {
-    toast.success("Event Deleted Successfully!", {
-      position: "bottom-right",
-      transition: Slide,
-    });
+  const inputFields = [
+    {
+      name: 'Username',
+      id: 'username',
+    },
+    {
+      name: 'Password',
+      id: 'password',
+    },
+  ];
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/checkAdmin`, data);
+      // Check the response data for isAdmin status
+      const { isAdmin } = response.data;
+      if (isAdmin) {
+        // Authentication successful, redirect to the dashboard page
+        router.push('/dashboard');
+      } else {
+        // Authentication failed, handle the error
+        console.error('User is not an admin');
+      }
+    } catch (error) {
+      // Handle any errors that occur during the authentication process
+      console.error('Error during authentication:', error);
+    }
   };
 
   return (
-    <Layout>
-      <div className="dashboard-container">
-        <div className="dashContainer">
-          Today's Schedule
-          <div className="innerContainer">
-            <Event
-              eventSchedule={() => setEventSchedule(!eventSchedule)}
-              events={events.filter((event) => {
-                const eventDate = event.time.split("T")[0];
-                return eventDate === todaysDate;
-              })}
-              onUpdate={() => setOnUpdate(!onUpdate)}
-              showDeleteEventToast={showDeleteEventToast}
-            />
-          </div>
+    <div className={login.main}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={login.formContainer}>
+          {inputFields.map(({ name, type, id, placeholder }) => (
+            <div key={name} className={login.inputContainer}>
+              <input
+                key={name}
+                id={id}
+                {...register(id, { required: true })}
+                className={login.inputLogin}
+                placeholder={name}
+              />
+              <span className={login.error}>{errors[id] && (
+                name + ' is required' 
+              )}</span>
+            </div>
+          ))}
         </div>
-      </div>
-      <ToastContainer />
-    </Layout>
+        <div className={login.buttonContainer}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default IndexPage;
-
-export const Head = () => <title>VHD</title>;
+export default LoginPage;
