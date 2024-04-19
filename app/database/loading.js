6fcb0ad96a06755449bd6c9756cdcb18a560feb2
oelsheha <oelsheha@sfu.ca>
2024-04-19 09:58:49 -0700

    style: removed loading skeleton for unauthorized user

--- app/database/loading.js
+++ app/database/loading.js
@@ -1,5 +1,12 @@
 import { TableContainerSkeleton } from "../../components/Skeletons";
+import { auth } from "@clerk/nextjs";
 
 export default function Loading() {
-    return <TableContainerSkeleton />
+  const { has } = auth();
+
+  const canManage = has({ permission: "org:database:allow" });
+
+  if (!canManage) return null;
+
+  return <TableContainerSkeleton />;
 }
