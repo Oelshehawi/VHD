ae785b93501713ce097ed0354940e62f912a5009
oelsheha <oelsheha@sfu.ca>
2024-05-25 04:10:36 -0700

    fix: added redirect for / route and after auth function to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
 
-export default authMiddleware({});
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // Redirect signed in users to organization selection page if they are not active in an organization
+    // If the user is signed in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
